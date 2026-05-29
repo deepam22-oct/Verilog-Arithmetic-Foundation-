df2941dc00a4619ee2d7361c562b026b3ef93853
Deepam kumar Bisoyi <127032868+deepam22-oct@users.noreply.github.com>
2026-05-29 13:14:00 +0530

    Define top module for 16-bit carry select adder
    Added top module definition for 16-bit carry select adder.

--- CARRY SELECT ADDER/design/carry_select_adder_16bit.v
+++ CARRY SELECT ADDER/design/carry_select_adder_16bit.v
@@ -24,3 +24,15 @@ module rca_4bit (
   full_adder fa4(a[3],b[3],c[2],sum[3],cout);
 
 endmodule
+
+// ---------- CARRY SELECT ADDER TOP MODULE----------------
+
+module carry_select_adder_16bit(
+  input [15:0]a,b,
+  input cin,
+  output [15:0]sum,
+  output cout
+);
+  
+
+endmodule
