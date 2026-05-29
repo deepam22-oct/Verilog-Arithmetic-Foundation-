bb9fcc286baab84565975cc2e98733c9efb16036
Deepam kumar Bisoyi <127032868+deepam22-oct@users.noreply.github.com>
2026-05-29 12:44:54 +0530

    Add 4-bit ripple carry adder module

--- CARRY SELECT ADDER/design/carry_select_adder_16bit.v
+++ CARRY SELECT ADDER/design/carry_select_adder_16bit.v
@@ -8,3 +8,19 @@ module full_adder(
 
 endmodule
 
+// ------------ RIPPLE CARRY ADDER 4 BIT -------------------
+module rca_4bit (
+  input [3:0]a,b,
+  input cin ,
+  output [3:0] sum ,
+  output cout 
+);
+
+  wire [2:0]c ;
+
+  full_adder fa1(a[0],b[0],cin,sum[0],c[0]);
+  full_adder fa2(a[1],b[1],c[0],sum[1],c[1]);
+  full_adder fa3(a[2],b[2],c[1],sum[2],c[2]);
+  full_adder fa4(a[3],b[3],c[2],sum[3],cout);
+
+endmodule
